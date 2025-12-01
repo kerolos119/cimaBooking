3b831b820d734fa1939dfbe15d604d243a28cd15
kerolos119 <kerolosbotros99@gmail.com>
2025-12-01 18:08:53 +0200

    Initial commit

---
+++ src/main/java/com/example/cimaBooking/CimaBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cimaBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CimaBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CimaBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cimaBooking/CimaBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cimaBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CimaBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
